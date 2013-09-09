95b93438f9c4d042090958622b845a7007085632
Kevin Sandness <ksandness@gmail.com>
2013-09-09 11:37:52 -0700

    Initialzie repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '825ec27699b7b8dd9d8280ef1b9e6dd64c634f481cfa7847f1f985bf0a71ffe27b9a5391de7fb4faad1b1fd5a5f10d32ea0bb28e44e9269a3fb09094f1b2d9cc'
